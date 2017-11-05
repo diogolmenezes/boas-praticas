552d0d1b39f2dee9e471cdba3c672d8a9b832f6e
Diogo Menezes <diogolmenezes@gmail.com>
2017-11-05 18:15:57 -0200

    ajustes

--- token/OiToken.js
+++ token/OiToken.js
@@ -1,11 +1,31 @@
 // OiToken.js
 class OiToken {
-    gerar() {
-        // por ser um exemplo, não levamos em consideração como esse numero é gerado
-        // precisamos apenas de um numero aleatorio de 6 caracteres
-        var token = String(parseInt(Math.random() * (999999 - 111111) + 111111));
+    constructor() {
+        this.enviarMensagem = require('./soap/enviarMensagem');
+    };
 
-        return token;
+    gerar() {
+        return new Promise((resolve, reject) => {
+            try {
+                // por ser um exemplo, não levamos em consideração como esse numero é gerado
+                // precisamos apenas de um numero aleatorio de 6 caracteres
+                var token = String(parseInt(Math.random() * (999999 - 111111) + 111111));
+
+                resolve(token);
+
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+
+    enviar(numero) {
+        var base = this;
+
+        return this.gerar()
+            .then(token => {
+                return base.enviarMensagem.enviarSms(token, numero);
+            })
     };
 };
 
--- token/test/unitario.js
+++ token/test/unitario.js
@@ -1,5 +1,6 @@
 // test/unitario.js
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 // criando uma sessão para os testes da classe token
 describe('OiToken', function () {
@@ -14,20 +15,117 @@ describe('OiToken', function () {
         });
 
         it('Deve retornar uma string de 6 caracteres', function (done) {
-            expect(oitoken.gerar().length).to.be.equal(6);
-            done();
+            oitoken.gerar()
+                .then(token => {
+                    expect(token.length).to.be.equal(6);
+                    done();
+                });
         });
 
         it('Deve gerar tokens diferentes', function (done) {
-            let primeira = oitoken.gerar();
-            let segunda = oitoken.gerar();
-            let terceira = oitoken.gerar();
 
-            expect(primeira).to.not.be.equal(segunda);
-            expect(primeira).to.not.be.equal(terceira);
-            expect(segunda).to.not.be.equal(terceira);
+            let primeira, segunda, terceira;
 
+            oitoken.gerar()
+                .then(token => {
+                    primeira = token;
+                    return oitoken.gerar();
+                })
+                .then(token => {
+                    segunda = token;
+                    return oitoken.gerar();
+                })
+                .then(token => {
+                    terceira = token;
+
+                    expect(primeira).to.not.be.equal(segunda);
+                    expect(primeira).to.not.be.equal(terceira);
+                    expect(segunda).to.not.be.equal(terceira);
+
+                    done();
+                });
+        });
+
+        it('Deve gerar tokens com apenas números', function (done) {
+            oitoken.gerar()
+                .then(token => {
+                    primeira = token;
+                    return oitoken.gerar();
+                })
+                .then(token => {
+                    segunda = token;
+                    return oitoken.gerar();
+                })
+                .then(token => {
+                    terceira = token;
+
+                    expect(primeira).to.match(/^[0-9]{6}$/);
+                    expect(segunda).to.match(/^[0-9]{6}$/);
+                    expect(terceira).to.match(/^[0-9]{6}$/);
+
+                    done();
+                });
+        });
+
+    });
+
+    describe('Enviar', function () {
+
+        let sandbox, enviarMensagemStub;
+
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create();
+
+            // criando um stub para o metodo enviar da classe oitoken.enviarMensagemService
+            // dessa forma garantimos que o teste unitario não dependerá de nada externo.
+            enviarMensagemStub = sandbox.stub(oitoken.enviarMensagem, 'enviarSms').callsFake(() => {
+                return Promise.resolve('SMS foi enviado...');
+            });
+        });
+
+        afterEach(() => {
+            sandbox.restore();
+        });
+
+        it('Deve possuir o método enviar', function (done) {
+            expect(oitoken).to.have.property('enviar');
             done();
+        });
+
+        it('Deve chamar o método de geração de token', function (done) {
+            let spyGerar = sandbox.spy(oitoken, 'gerar');
+            oitoken.enviar()
+                .then(() => {
+                    expect(spyGerar.calledOnce).to.be.ok;
+                    done();
+                });
+        });
+
+        it('Deve chamar o método de envio de sms', function (done) {
+            oitoken.enviar()
+                .then(() => {
+                    expect(enviarMensagemStub.calledOnce).to.be.ok;
+                    done();
+                });
+        });
+
+        it('Deve chamar o método de envio de sms com os parâmetros corretos', function (done) {
+            oitoken.enviar('21999999999')
+                .then(() => {
+                    // testa se o metodo de envio de SMS esta sendo chamado com um token e com o número de telefone correto.
+                    expect(enviarMensagemStub.calledWith(sinon.match(/^[0-9]{6}$/), '21999999999')).to.be.ok;
+                    done();
+                });
+
+        });
+
+        it('Deve respeitar o fluxo correto', function (done) {
+            let spyGerar = sandbox.spy(oitoken, 'gerar');
+            oitoken.enviar('219999999999')
+                .then(() => {
+                    expect(sinon.assert.callOrder(spyGerar, enviarMensagemStub));
+                    done();
+                });
         });
     });
 });
